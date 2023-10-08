fb781d17aa15b3a58b448fb8bf42f2dc685efd90
Bima Febriansyah <bimafebriansyah1002@gmail.com>
2023-10-08 22:46:37 +0700

    change port

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import cors from 'cors'
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
 
 app.use(async (_req, _res, next) => {
   await db.sequelize.authenticate()
